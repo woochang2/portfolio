b89705f02bf2822601a2560981a16d90f91d1a5f
Jeong Woo Chang <wcjeong@postech.ac.kr>
2022-07-24 21:19:33 +0900

    coloring wave

--- js/wavegroup.js
+++ js/wavegroup.js
@@ -5,7 +5,7 @@ export class WaveGroup {
         this.totalWaves = 3;
         this.totalPoints = 6;
 
-        this.color = ['rgba(0,199,235,0,4)', 'rgba(0,146,199,0,4)', 'rgba(0,87,158,0,4)'];
+        this.color = ['rgba(255,0,0,0.4)', 'rgba(255,255,0,0.4)', 'rgba(0,255,255,0.4)'];
 
         this.waves = [];
 
