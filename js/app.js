1d2c23b990c60f1fe6c1308b9b13b47cc478ffed
Jeong Woo Chang <wcjeong@postech.ac.kr>
2022-07-24 21:58:42 +0900

    wave world

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { WaveGroup } from './wavegroup.js';
 
 class App{
     constructor() {
-        this.canvas = document.getElementById('canvas');
+        this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
 
